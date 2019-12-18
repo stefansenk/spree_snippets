88c5f540bd88bcbe0744ee71d5b7e636f598b634
Stefan Senk <stefan@senktec.com>
2019-12-18 10:33:25 +0000

    Update Spree version to 4

--- app/controllers/spree/store_controller_decorator.rb
+++ app/controllers/spree/store_controller_decorator.rb
@@ -1,5 +1,5 @@
-if defined? Spree::StoreController
-  Spree::StoreController.class_eval do
-    helper Spree::SnippetHelper
+module Spree::StoreControllerDecorator
+  if defined? Spree::StoreController
+    Spree::StoreController.helper Spree::SnippetHelper
   end
 end
