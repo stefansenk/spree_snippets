573ec3a4a426435f56f27a254ba5e3bd1ff7db18
Stefan Senk <stefan@senktec.com>
2020-04-10 10:26:09 +0100

    Updates to admin sidebar.

--- app/overrides/add_snippets_admin_tab.rb
+++ app/overrides/add_snippets_admin_tab.rb
@@ -1,6 +1,15 @@
-Deface::Override.new(
-  virtual_path: 'spree/layouts/admin',
-  name: 'snippet_admin_configurations_menu',
-  insert_bottom: '#main-sidebar',
-  partial: 'spree/admin/shared/snippets_sidebar_menu'
-)
+if Spree.version.to_f < 4.0
+ Deface::Override.new(
+   virtual_path: 'spree/layouts/admin',
+   name: 'snippet_admin_configurations_menu',
+   insert_bottom: '#main-sidebar',
+   partial: 'spree/admin/shared/snippets_sidebar_menu'
+ )
+else
+ Deface::Override.new(
+   virtual_path: 'spree/admin/shared/_main_menu',
+   name: 'snippet_admin_configurations_menu',
+   insert_bottom: 'nav',
+   partial: 'spree/admin/shared/snippets_sidebar_menu'
+ )
+end
